31c50f7629fd6660ff2a7724fc28aea7d00687b4
madinabolat <madinabolat01@gmail.com>
2025-12-09 22:13:49 -0500

    add a file reader class

---
+++ huffman/src/main/java/org/example/FileReader.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReader {
+
+    public void readFile(){
+        String pathToFile = "../text.txt";
+        File file = new File(pathToFile);
+        String s = new String();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()){
+                s = s.concat(scanner.nextLine());
+            }
+            scanner.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(s);
+    }
+
+}
--- huffman/src/main/java/org/example/Main.java
+++ huffman/src/main/java/org/example/Main.java
@@ -1,8 +1,33 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+//        String pathToFile = "/Users/madinabolat/Downloads/dataset_91069.txt";
+//        File file = new File(pathToFile);
+//        int count = 0;
+//        try {
+//            Scanner scanner = new Scanner(file);
+//
+//            if (scanner.hasNext()){
+//                scanner.nextLine();
+//            } else {
+//                return;
+//            }
+//            while (scanner.hasNext()){
+//                System.out.println(scanner.nextInt());
+//            }
+//            scanner.close();
+//        }
+//        catch(FileNotFoundException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        System.out.println(count);
+
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
     }
 }
