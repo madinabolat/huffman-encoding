09e3a86dff096dd26abf58724e3c7284614a237f
madinabolat <madinabolat01@gmail.com>
2026-01-15 22:08:10 -0500

    files

--- huffman/src/main/java/org/example/FileOperator.java
+++ huffman/src/main/java/org/example/FileOperator.java
@@ -1,6 +1,26 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileOperator {
-    //read from file
-    //write to file
+
+    public String readFromFile(String path) throws FileNotFoundException {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        String text = new String();
+        while (scanner.hasNextLine()){
+            text += scanner.nextLine();
+        }
+        return text;
+    }
+
+    public void writeToFile(String s) {
+        //create new file
+        //write to file
+        //return path to file? 
+
+    }
+
 }
--- huffman/src/main/java/org/example/Main.java
+++ huffman/src/main/java/org/example/Main.java
@@ -1,12 +1,25 @@
 package org.example;
 
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) {
-        String s = "ABRACADABRA";
-        HuffmanTree ht = new HuffmanTree(s);
+    public static void main(String[] args) throws FileNotFoundException {
+//        String s = "ABRACADABRA";
+//        HuffmanTree ht = new HuffmanTree(s);
+//        ht.preOrderTraverse(ht.root);
+//        //System.out.println(ht.encodeChar('C', ht.root));
+//        HashMap<Character, String> encodingDict = ht.buildEncodingDict(s);
+//        System.out.println(encodingDict);
+//        System.out.println(ht.encodeString(s));
+//        System.out.println(ht.decodeString(ht.encodeString(s)));
+
+        String path = "../input.txt";
+        FileOperator fop = new FileOperator();
+        String s = fop.readFromFile(path);
+        System.out.println(s);
+                HuffmanTree ht = new HuffmanTree(s);
         ht.preOrderTraverse(ht.root);
         //System.out.println(ht.encodeChar('C', ht.root));
         HashMap<Character, String> encodingDict = ht.buildEncodingDict(s);
