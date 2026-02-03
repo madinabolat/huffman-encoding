6b3dceb50e9048b2127ad44fece3796631405935
madinabolat <madinabolat01@gmail.com>
2026-02-02 22:11:15 -0500

    add cli commands to main

--- huffman/src/main/java/org/example/Main.java
+++ huffman/src/main/java/org/example/Main.java
@@ -5,8 +5,13 @@ import java.nio.ByteBuffer;
 public class Main {
     public static void main(String[] args) {
         HuffmanCompressor huffmanCompressor = new HuffmanCompressor();
-        huffmanCompressor.compress("../input.txt", "../encoded.hf");
-        huffmanCompressor.decompress("../encoded.hf", "../output.txt");
 
+        if (args[0].equals("compress")){
+            huffmanCompressor.compress(args[1], args[1].replace(".txt", "")+"_encoded.hf");
+        } else if (args[0].equals("decompress")){
+            huffmanCompressor.decompress(args[1], args[1].replace(".hf", "")+"_decoded.txt");
+        } else {
+            System.out.println("Enter valid function: compress or decompress");
+        }
     }
 }
